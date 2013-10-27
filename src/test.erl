50d2b5ac43d4b5093f14b38ae62f185cfc2d0120
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-10-27 13:21:16 -0700

    Update tests.

--- src/test.erl
+++ src/test.erl
@@ -1,25 +1,41 @@
 -module(test).
 -export([test/0]).
 
+%% @doc Generate a test vector clock.
 test() ->
     Fresh = vvclock:fresh(),
-    vvclock:increment(1, Fresh),
+    io:format("Fresh: ~p~n", [Fresh]),
+    Incremented = vvclock:increment(1, Fresh),
+    io:format("Incremented: ~p~n", [Incremented]),
+
+    %% Run the vector clock test from Riak Core.
     example_test().
 
-% From Riak Core.
+%% @doc Take from riak_core/src/vclock.erl.
 example_test() ->
-    A = vvclock:fresh(),
-    B = vvclock:fresh(),
-    A1 = vvclock:increment(a, A),
-    B1 = vvclock:increment(b, B),
-    'True' = vvclock:descends(A1,A),
-    'True' = vvclock:descends(B1,B),
-    'False' = vvclock:descends(A1,B1),
-    A2 = vvclock:increment(a, A1),
-    C = vvclock:merge([A2, B1]),
-    C1 = vvclock:increment(c, C),
-    'True' = vvclock:descends(C1, A2),
-    'True' = vvclock:descends(C1, B1),
+    A  = vvclock:fresh(),
+    B  = vvclock:fresh(),
+    A1 = vvclock:increment('O', A),
+    B1 = vvclock:increment({'S', 'O'}, B),
+    B2 = vvclock:increment({'S', 'O'}, B1),
+
+    io:format("A:  ~p~n", [A]),
+    io:format("B:  ~p~n", [B]),
+    io:format("A1: ~p~n", [A1]),
+    io:format("B1: ~p~n", [B1]),
+    io:format("B2: ~p~n", [B2]),
+
+    'True'  = vvclock:descends(A1, A),
+    'True'  = vvclock:descends(B1, B),
+    'False' = vvclock:descends(A1, B1),
+
+    A2 = vvclock:increment('O', A1),
+    C  = vvclock:merge([A2, B1]),
+    C1 = vvclock:increment({'S', {'S', 'O'}}, C),
+
+    'True'  = vvclock:descends(C1, A2),
+    'True'  = vvclock:descends(C1, B1),
     'False' = vvclock:descends(B1, C1),
     'False' = vvclock:descends(B1, A1),
+
     ok.
