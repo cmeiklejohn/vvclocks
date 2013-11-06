3a76dabb6a033a68ba4d4475c9f96816ab047532
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-11-06 13:30:38 -0500

    Extend the model with timestamps.

--- coq/vvclock.v
+++ coq/vvclock.v
@@ -4,11 +4,18 @@ Require Import Coq.Bool.Bool.
 
 Module VVClock.
 
+(** Type definitions for actors, counts and timestamps. *)
 Definition actor := nat.
 Definition count := nat.
-Definition clock := (actor * count)%type.
-Definition vclock := list clock%type.
+Definition timestamp := nat.
 
+(** Model clocks as triples. *)
+Definition clock := prod actor (prod count timestamp).
+
+(** Model vector clocks at a list of clock triples. *)
+Definition vclock := (list clock)%type.
+
+(** Create a new vector clocks. *)
 Definition fresh : vclock := nil.
 
 Definition find' (actor : actor) :=
@@ -21,27 +28,46 @@ Definition find'' (actor : actor) :=
                            | pair x _ => negb (beq_nat actor x)
                        end.
 
+(** Function to initialize a timestamp with the default value. *)
+Definition init_timestamp := O.
+
+(** Function to increment a timestamp from one value to the next. *)
+Definition incr_timestamp (timestamp : timestamp) := S timestamp.
+
+(** Function to initialize a counter with the default values. *)
+Definition init_count := O.
+
+(** Function to increment the counter from one value to the next. *)
+Definition incr_count (count : count) := S count.
+
+(** Increment a vector clock. *)
 Definition increment (actor : actor) (vclock : vclock) :=
   match find (find' actor) vclock with
   | None => 
-    cons (pair actor 1) vclock
-  | Some (pair x y) => 
-    cons (pair x (S y)) (filter (find' actor) vclock)
+    cons (pair actor (pair init_count init_timestamp)) vclock
+  | Some (pair x (pair count timestamp)) => 
+    cons (pair x (pair (incr_count count) (incr_timestamp timestamp)))
+                       (filter (find' actor) vclock)
   end.
 
+(** Helper fold function for equality comparison betwen two vector clocks. *)
 Definition equal' status_and_vclock (clock : clock) :=
   match clock, status_and_vclock with
-    | pair actor count, 
+    | pair actor (pair count timestamp), 
       pair status vclock => match find (find' actor) vclock with
                               | None => 
                                 pair false vclock
-                              | Some (pair _ y) => 
-                                pair (andb
+                              | Some (pair _ (pair y z)) => 
+                                pair (andb 
                                         status
-                                        (beq_nat count y)) vclock
+                                        (andb
+                                           (beq_nat count y)
+                                           (beq_nat timestamp z)))
+                                        vclock
                             end
   end.
 
+(** Compare equality between two vector clocks. *)
 Definition equal (vc1 vc2 : vclock) := 
   match fold_left equal' vc1 (pair true vc2) with
     | pair false _ => 
@@ -55,6 +81,7 @@ Definition equal (vc1 vc2 : vclock) :=
       end
   end.
 
+(** Less than or equal to comparson for natural numbers. *)
 Fixpoint ble_nat (n m : nat) {struct n} : bool :=
   match n with
   | O => true
@@ -65,19 +92,23 @@ Fixpoint ble_nat (n m : nat) {struct n} : bool :=
       end
   end.
 
+(** Decends fold helper for determining ordering. *)
 Definition descends' status_and_vclock (clock : clock) :=
   match clock, status_and_vclock with
-    | pair actor count,
+    | pair actor (pair count timestamp),
       pair status vclock => match find (find' actor) vclock with
                               | None => 
                                 pair false vclock
-                              | Some (pair _ y) => 
+                              | Some (pair _ (pair y z)) => 
                                 pair (andb
                                         status
-                                        (ble_nat count y)) vclock
-                                                               end
+                                        (andb
+                                           (ble_nat count y)
+                                           (ble_nat timestamp z))) vclock
+                            end
   end.
 
+(** Determine if one vector clock is a descendent of another. *)
 Definition descends (vc1 vc2 : vclock) := 
   match fold_left descends' vc2 (pair true vc1) with
     | pair false _ =>
@@ -86,27 +117,41 @@ Definition descends (vc1 vc2 : vclock) :=
       true
   end.
 
+(** Fold helper for the merge function which computes max. *)
 Definition max' (vclock : vclock) (clock : clock) :=
   match clock with
-    | pair actor count =>  match find (find' actor) vclock with
-                             | None => 
-                               cons (pair actor count) vclock
-                             | Some (pair _ y) => 
-                               cons (pair actor (max count y))
-                                    (filter (find'' actor) vclock)
-                           end
+    | pair actor (pair count timestamp) => 
+      match find (find' actor) vclock with
+        | None => 
+          cons (pair actor (pair count timestamp)) vclock
+        | Some (pair _ (pair y z)) => 
+          cons (pair actor (pair (max count y) (max timestamp z)))
+               (filter (find'' actor) vclock)
+      end
   end.
 
+(** Merge two vector clocks. *)
 Definition merge (vc1 vc2 : vclock) := fold_left max' vc1 vc2.
 
+(** Return current count of an actor in a vector clock. *)
 Definition get_counter (actor : actor) (vclock : vclock) :=
   match find (find' actor) vclock with
       | None => 
         None
-      | Some (pair a vc) =>
-        Some vc
+      | Some (pair a (pair count timetsamp)) =>
+        Some count
   end.
 
+(** Return current timestamp of an actor in a vector clock. *)
+Definition get_timestamp (actor : actor) (vclock : vclock) :=
+  match find (find' actor) vclock with
+      | None => 
+        None
+      | Some (pair a (pair count timetsamp)) =>
+        Some timestamp
+  end.
+
+(** Return all actors who have ever incremented the vector clock. *)
 Fixpoint all_nodes (vclock : vclock) :=
   match vclock with
     | nil => 
@@ -117,20 +162,25 @@ Fixpoint all_nodes (vclock : vclock) :=
       end
   end.
 
+(** Proof that the merge function is idempotent. *)
 Theorem merge_idemp : forall vc1, merge vc1 vc1 = vc1.
 Proof. Admitted.
 
+(** Proof that the merge function is commutative. *)
 Theorem merge_comm : forall vc1 vc2, merge vc1 vc2 = merge vc2 vc1.
 Proof. Admitted.
 
+(** Proof that the merge function is associatative. *)
 Theorem merge_assoc : forall vc1 vc2 vc3, 
                         merge vc1 (merge vc2 vc3) = merge (merge vc1 vc2) vc3.
 Proof. Admitted.
 
+(** Proof that the increment function over vector clocks increments. *)
 Theorem vclock_increment : forall (actor : actor) (vclock : vclock),
   descends (increment actor vclock) vclock = true.
 Proof. Admitted.
 
+(** Proof that the vector clock preserves an antisymmetric relation. *)
 Theorem vclock_antisymmetric : forall (actor : actor) (vclock : vclock),
   descends (increment actor vclock) vclock = 
     negb (descends vclock (increment actor vclock)).
